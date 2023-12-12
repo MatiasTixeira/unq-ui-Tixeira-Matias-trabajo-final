c340becd3fe995e6b84d3bdf3702bf8ae68b3cc1
MatiasTixeira <71102765+MatiasTixeira@users.noreply.github.com>
2023-12-12 11:13:02 -0300

    Delete src/components/CuadradoDelTablero.jsx

--- src/components/CuadradoDelTablero.jsx
+++
@@ -1,9 +0,0 @@
-const CuadradoDelTablero = ({ value, onClick }) => {
-  return (
-    <button className="square" onClick={onClick}>
-      {value}
-    </button>
-  )
-}
-
-export default CuadradoDelTablero
